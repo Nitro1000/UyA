00fe03dd629e72c1790f050fa0730dc9f61f79b3
Juan Carlos Vega <50880962+alu0100256628@users.noreply.github.com>
2020-06-02 21:00:05 +0100

    Add files via upload

--- Practica6/js/ajax.js
+++ Practica6/js/ajax.js
@@ -1,48 +1,26 @@
-	$("#boton0").click(function(e){
-		$.ajax({
-			url:`https://jsonplaceholder.typicode.com/comments?postId=${$("#valorPostid").val()}`,
-			type: "get",
-			dataType: "json",
-			headers: {'Access-Control-Allow-Origin': '*'},
-			async: true,
-			
-			success: function(result){
-				
-					for(let i=0;i<result.length;i++){
-						$('#Postid1').append(` name : ${result[i].name} email: ${result[i].email}<br><br>`);
-					}
 
-			},
-			error: function(...result){
-				alert('Error')	;
-				}	
-		})
-	});	
-	
+$('#play').click(() => {
+    if ($('#post_id').val()) {
+        var param = $("#formulario").serialize();
+        $.get("https://jsonplaceholder.typicode.com/comments", param, (data) => {
+            let texto = `<div><p>`;
+            for (let i in data) {
+                texto += `
+                    <li>
+                        <div >ID: ${data[i].id}</div>
+                        <div class="left-align">
+                            <span>ID: ${data[i].name}</span><br>
+                            <span>Email: ${data[i].email}</span><br>
+                            <span>Body: ${data[i].body}</span><br>
+                        </div>
+                    </li>`
+            }
+            texto += `</p></div>`;
+            $('#write').html(texto);
 
-	
-
-	$("#boton").click(function(e){
-		$.ajax({
-			url:`https://jsonplaceholder.typicode.com/comments?postId=1&id=${$("#valor").val()}`,
-			type: "get",
-			dataType: "json",
-			headers: {'Access-Control-Allow-Origin': '*'},
-			async: true,
-			
-			success: function(result){
-				
-				if ($("#valor").val() < 6 && $("#valor").val() > 0 ) {
-					for(let i=0;i<result.length;i++){
-						$('#exito').append(` name : ${result[i].name} email: ${result[i].email}<br><br>`);
-					}
-				} else {
-					document.getElementById("fracaso").innerHTML = "Id fuera de rango";
-				}
-			},
-			error: function(...result){
-				alert('Error')	;
-				}	
-		})
-	});	
-
+            $(document).ready(function () {
+                $('.collapsible').collapsible();
+            });
+        });
+    }
+});
